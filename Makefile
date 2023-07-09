b058ee1cf8e9ce89cc2140b8de240728e0332a81
Blessing Pariola <blessingpariola@zoho.com>
2023-07-10 00:00:49 +0200

    feat: broadcast challenge I

--- Makefile
+++ Makefile
@@ -12,3 +12,9 @@ build-2: clean
 
 test-2: build-2
 	maelstrom test -w unique-ids --bin ./bin/main --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition
+
+build-3a: clean
+	go build -o ./bin/main broadcast-a/main.go
+
+test-3a: build-3a
+	maelstrom test -w broadcast --bin ./bin/main --node-count 1 --time-limit 20 --rate 10
---
+++ broadcast-a/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+type broadcastMsg struct {
+	Message int `json:"message"`
+}
+
+type topologyMsg struct {
+	Topology map[string][]string `json:"topology"`
+}
+
+func main() {
+	n := maelstrom.NewNode()
+
+	messages := make([]int, 0)
+	// neighbours := make([]string, 0)
+
+	n.Handle("topology", func(msg maelstrom.Message) error {
+		var body topologyMsg
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		// update neighbours
+		// neighbours = body.Topology[n.ID()]
+
+		response := map[string]any{"type": "topology_ok"}
+		return n.Reply(msg, response)
+	})
+
+	n.Handle("broadcast", func(msg maelstrom.Message) error {
+		var body broadcastMsg
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		messages = append(messages, body.Message)
+
+		response := map[string]string{"type": "broadcast_ok"}
+		return n.Reply(msg, response)
+	})
+
+	n.Handle("read", func(msg maelstrom.Message) error {
+		response := map[string]any{
+			"type":     "read_ok",
+			"messages": messages,
+		}
+		return n.Reply(msg, response)
+	})
+
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
